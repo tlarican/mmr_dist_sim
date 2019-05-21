faf93aa28ae344c9ad9cf6b73a979dff257a9e65
conmano <43185357+conmano@users.noreply.github.com>
2019-05-21 14:20:09 -0700

    Created Player.py

---
+++ Player.py
@@ -0,0 +1,10 @@
+import numpy as N
+
+class Player(object):
+    """Create and manage Player variables and attributes
+    
+        Variables:
+            PlayerSkil
+            Tilt: Random variable to determine player's anger
+            
+                
