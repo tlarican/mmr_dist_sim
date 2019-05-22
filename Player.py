4487c87b0c427a7cceb9727664ffa94b14b51988
tlarican <46582411+tlarican@users.noreply.github.com>
2019-05-22 11:45:20 -0700

    added variables based off Antong's matches needs

--- Player.py
+++ Player.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-
+#  TODO(Tyler): Create Player Tests
 class Player(object):
     """Create and manage Player variables and attributes
     
@@ -12,3 +12,8 @@ class Player(object):
     def __init__(self, mmr_default=1400):
         self.skill = np.random.normal(5, 2)
         self.mmr = mmr_default
+        self.has_played = False
+        #  TODO(Antong): Make this variable a random number based on what you believe the distribution should be
+        self.is_online = True
+        #  TODO(Tyler): Make this based off the buckets that will be defined in the Model Object
+        self.rank = None
