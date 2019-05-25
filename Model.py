0f0c4e5ec76f3455a1a5c5b71121ce5a67db224b
conmano <43185357+conmano@users.noreply.github.com>
2019-05-24 17:07:36 -0700

    Update Model
    Added comments and documentation in the file. Also added (commented out) the new graphing function.

--- Model.py
+++ Model.py
@@ -1,15 +1,26 @@
+#=======================================================================
+#                           General Documentation
+
+"""Module that contains the Model class
+
+    see function docstring for description
+"""
+
+#---------------- Module General Import and Declarations ---------------
+
 import numpy as np
-
 from Player import Player
-from Graphing import showRanksUnsorted
+import Graphing 
 
+#-------------------- Class: Model ------------------------------------
 
 class Model(object):
     def __init__(self, number_players=1000):
         self.player_list = []
         for i in range(number_players):
             self.player_list.append(Player())
-        showRanksUnsorted(self.player_list)
+        #Graphing.showRanksUnsorted(self.player_list)
+        #Graphing.showMMR(self.player_list)
         self.rank_mins = []
         
 
