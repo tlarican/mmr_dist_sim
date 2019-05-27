b2ff26c22d617daac8a6816818b710c7c1dc807f
TLarican <tlarican@uw.edu>
2019-05-27 02:14:04 -0700

    Documentation
    Took 27 seconds

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,6 +1,20 @@
+# =======================================================================
+#                           General Documentation
+
+"""
+Tests for Model Class
+"""
+
+# ---------------- Module General Import and Declarations ---------------
 from Model import Model
 
+
 class Tests(object):
+    """
+    Testing object for when test.py is ran through main
+    """
     def test_model_init(self):
         model = Model()
-        self.assertTrue(model._test_player_list_size() == 1000)
+        self.assertEqual(model._test_player_list_size(), 1000)
+
+
