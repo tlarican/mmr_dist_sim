effc34de2c94bb5dc3908c882809a13fd6a89e19
conmano <43185357+conmano@users.noreply.github.com>
2019-05-26 19:28:14 -0700

    Added handleMatchResults

--- server.py
+++ server.py
@@ -19,11 +19,143 @@ def match(players):
         ------
             returns 1 if team 1 wins, otherwise 2
     """
+    """
+    #-Adds together the players skills that affect the early game for both teams
+    
+    team_1_earlyGame = np.average(players[:TEAM_SIZE].skill + players[:TEAM_SIZE].lastHitting + \
+        players[:TEAM_SIZE].tilt + players[:TEAM_SIZE].waveManagement + players[:TEAM_SIZE].mechanics)
+        
+    team_2_earlyGame = np.average(players[TEAM_SIZE:].skill + players[TEAM_SIZE:].lastHitting + \
+        players[TEAM_SIZE:].tilt + players[TEAM_SIZE:].waveManagement + players[TEAM_SIZE:].mechanics)
+    
+    
+    #-Sets the percent chance to win by dividing it by the amount of skills * 10
+    
+    team_1_earlyGame = team_1_earlyGame / 50
+    team_2_earlyGame = team_2_earlyGame / 50
+    
+    
+    #-
+    """
+    
+    #- Variable Declarations
+     
     team_1_odds = np.average(players[:TEAM_SIZE].skill)
     team_2_odds = np.average(players[TEAM_SIZE:].skill)
+    average_mmr = np.average(players[:].mmr)
+    winner = 1 + (team_2_odds > team_1_odds)
     
-    return 1 + (team_2_odds > team_1_odds)
+    
+    #- Calls handleMatchResults to deal with players
+    
+    handleMatchResults(players, winner, average_mmr)
+    
+    
+    #- Updates non rank/mmr/lp values
+    
+    for i in range(10):
+        if(players[i].amountOfGamesPlayed == 9):
+            players[i].rankUpMatch = True
+        players[i].has_played = True
+        players[i].amountOfGamesPlayed += 1                   
+        
+    return winner
 
+def handleMatchResults(players, winner, average_mmr):
+    """Takes the results of a match and adjust player mmr, lp, and rank
+    
+        Variables:
+            players = List of players in match
+            team_1_odds = The chances that team 1 would win
+            team_2_odds = The chances that team 2 would win
+            average_mmr = The average mmr of the match     
+    """
+    
+    if(winner == 2):
+        for i in range(TEAM_SIZE):
+            if(players[i].mmr < average_mmr):
+                players[i].mmr += 25
+                if(players[i].rankUpMatch == True):
+                    players[i].rankUp()
+                elif((players[i].lp + 23) > 100):
+                    players[i].lp = 100
+                    players.rankUpMatch = True
+                else:
+                    players[i].lp += 23
+            else:
+                players[i].mmr += 15
+                if(players[i].rankUpMatch == True):
+                    players[i].rankUp()
+                elif((players[i].lp + 18) > 100):
+                    players[i].lp = 100
+                    players.rankUpMatch = True
+                else:
+                    players[i].lp += 18
+        for i in range(10):
+            if(i < 5):
+                continue
+            if(players[i].mmr < average_mmr):
+                players[i].mmr -= 15
+                if(players[i].rankDownMatch == True):
+                    players[i].rankDownMatch()
+                elif((players[i].lp - 18) < 0):
+                    players[i].lp = 0
+                    players.rankDownMatch = True
+                else:
+                    players[i].lp -= 18
+            else:
+                players[i].mmr -= 25
+                if(players[i].rankDownMatch == True):
+                    players[i].rankDownMatch()
+                elif((players[i].lp - 23) < 0):
+                    players[i].lp = 0
+                    players.rankDownMatch = True
+                else:
+                    players[i].lp -= 23
+                    
+    else:
+        for i in range(10):
+            if(i < 5):
+                continue
+            if(players[i].mmr < average_mmr):
+                players[i].mmr += 25
+                if(players[i].rankUpMatch == True):
+                    players[i].rankUp()
+                elif((players[i].lp + 23) > 100):
+                    players[i].lp = 100
+                    players.rankUpMatch = True
+                else:
+                    players[i].lp += 23
+            else:
+                players[i].mmr += 15
+                if(players[i].rankUpMatch == True):
+                    players[i].rankUp()
+                elif((players[i].lp + 18) > 100):
+                    players[i].lp = 100
+                    players.rankUpMatch = True
+                else:
+                    players[i].lp += 18
+        for i in range(TEAM_SIZE):
+            if(players[i].mmr < average_mmr):
+                players[i].mmr -= 15
+                if(players[i].rankDownMatch == True):
+                    players[i].rankDownMatch()
+                elif((players[i].lp - 18) < 0):
+                    players[i].lp = 0
+                    players.rankDownMatch = True
+                else:
+                    players[i].lp -= 18
+            else:
+                players[i].mmr -= 25
+                if(players[i].rankDownMatch == True):
+                    players[i].rankDownMatch()
+                elif((players[i].lp - 23) < 0):
+                    players[i].lp = 0
+                    players.rankDownMatch = True
+                else:
+                    players[i].lp -= 23
+            
+    
 def pick_lobby(all_players):
     """
         Input
