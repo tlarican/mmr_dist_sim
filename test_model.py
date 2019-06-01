bf71d0e2d8167f9c74dc8afdcd915c4d71f99cc7
TLarican <tlarican@uw.edu>
2019-06-01 03:34:17 -0700

    Formatted
    Took 22 seconds

--- test_model.py
+++ test_model.py
@@ -13,8 +13,7 @@ class Tests(object):
     """
     Testing object for when test.py is ran through main
     """
+
     def test_model_init(self):
         model = Model()
         self.assertEqual(model._test_player_list_size(), 1000)
-
-
