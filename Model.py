5159ace4d2515c26e8d8026f939fe319dd4a83bc
TLarican <tlarican@uw.edu>
2019-06-01 03:31:59 -0700

    Commented Methods
    Took 30 seconds

--- Model.py
+++ Model.py
@@ -16,12 +16,21 @@ import Graphing
 # -------------------- Class: Model ------------------------------------
 
 class Model(object):
+    """
+    Mainly holds the players to run matches on.
+    """
     def __init__(self, number_players=1000):
+        """
+        Create list of player objects
+        :param number_players: Number of players in simulation
+        """
         self.player_list = []
         for i in range(number_players):
             self.player_list.append(Player())
-        self.rank_mins = []
 
     def _test_player_list_size(self):
+        """
+        Testing if amount of players initialized is correct
+        :return: amount of players initialized
+        """
         return len(self.player_list)
-
