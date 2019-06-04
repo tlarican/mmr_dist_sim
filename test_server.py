73c2a5a5a169c6073846d4a91a0c90a9a8505dbf
TLarican <tlarican@uw.edu>
2019-06-04 02:28:29 -0700

    test adjustments
    Took 31 seconds

--- test_server.py
+++ test_server.py
@@ -10,7 +10,7 @@ import server
 from Model import Model
 import Graphing
 
-SHOW_GRAPHS = True
+SHOW_GRAPHS = False
 
 
 class Tests(object):
@@ -32,23 +32,24 @@ class Tests(object):
         if SHOW_GRAPHS:
             Graphing.showMMR(model.player_list)
             Graphing.showRanksUnsorted(model.player_list)
+        model.to_csv()
 
     def test_match_winner_handling(self):
         players = server._test_match_winner_handling()
-        self.assertEqual(1019, players[0].mmr, msg='Win Below MMR')
-        self.assertEqual(15, players[0].lp, msg='Win Below LP')
-        self.assertEqual(2015, players[1].mmr, msg='Win Above MMR')
-        self.assertEqual(9, players[1].lp, msg='Win Above LP')
+        self.assertEqual(1517, players[0].mmr, msg='Win Below MMR')
+        self.assertEqual(87, players[0].lp, msg='Win Below LP')
+        self.assertEqual(1562, players[1].mmr, msg='Win Above MMR')
+        self.assertEqual(16, players[1].lp, msg='Win Above LP')
         self.assertEqual(100, players[2].lp, msg='At Rank Up LP')
         self.assertTrue(players[2].rankUpMatch, msg='At Rank Up Bool')
         self.assertEqual(5, players[3].rank, msg='Rank Up Rank')
         self.assertEqual(4, players[3].rankDivision, msg='Rank Up Division')
         self.assertEqual(0, players[3].lp, msg='Rank Up LP')
         self.assertFalse(players[3].rankUpMatch, msg='Rank Up Bool')
-        self.assertEqual(1985, players[5].mmr, msg='Lose Above MMR')
-        self.assertEqual(35, players[5].lp, msg='Lost Above LP')
-        self.assertEqual(989, players[6].mmr, msg='Player 6 MMR')
-        self.assertEqual(41, players[6].lp, msg='Player 6 LP')
+        self.assertEqual(1483, players[5].mmr, msg='Lose Above MMR')
+        self.assertEqual(0, players[5].lp, msg='Lost Above LP')
+        self.assertEqual(1438, players[6].mmr, msg='Lose Below MMR')
+        self.assertEqual(37, players[6].lp, msg='Lose Below LP')
         self.assertEqual(0, players[7].lp, msg='At Rank Down LP')
         self.assertTrue(players[7].rankDownMatch, msg='At Rank Down Bool')
         self.assertEqual(2, players[8].rank, msg='Rank Down Rank')
